44d5e1667df234255b208720e9b5572bfe7e9591
Leilton César <leilton.cesar@gmail.com>
2022-09-13 19:50:02 -0300

    feat: adding packages structures and cors

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 require('dotenv-flow').config();
+const morgan = require('morgan');
+
+app.use( (req, res, next ) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(morgan(`\x1b[33m Método=:method \x1b[36m Url=:url \x1b[35m Status=:status \x1b[34m Tempo:response-time ms \x1b[0m`));
 
 module.exports = app
