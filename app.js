efde781776d9d893f64e3253e7e2aa28cef3f7a5
Leilton César <leilton.cesar@gmail.com>
2022-09-14 12:33:52 -0300

    feat: adding swagger docs api

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const { setupRoutes } = require('./src/main/config/routes')
 
 require('./src/infra/config/database')
 
+const swagger = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 app.use( (req, res, next ) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -20,6 +23,22 @@ app.use( (req, res, next ) => {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const swaggerDefinition = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Api Challenge, about GoT and its houses',
+      version: '0.0.1',
+      description: 'Documentation api challenge for newtail'
+    }
+  },
+  apis: ['swagger.yaml']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerDefinition);
+
+app.use('/doc', swagger.serve, swagger.setup(swaggerDocs));
 
 app.use(morgan(`\x1b[33m Método=:method \x1b[36m Url=:url \x1b[35m Status=:status \x1b[34m Tempo:response-time ms \x1b[0m`));
 
